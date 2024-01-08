c2881d0aafdccbcc1f2d1a63c6089f00136380f4
Braden <4bktandme@gmail.com>
2024-01-07 22:16:38 -1000

    Update for 2024

--- FRC-2024-Crescendo/src/main/java/frc/robot/Commands/TeleopSwerve.java
+++ FRC-2024-Crescendo/src/main/java/frc/robot/Commands/TeleopSwerve.java
@@ -8,10 +8,10 @@ import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
 
 
-public class TeleopSwerve extends CommandBase {    
+public class TeleopSwerve extends Command {    
     protected Swerve swerve;    
     protected DoubleSupplier translationSup;
     protected DoubleSupplier strafeSup;
