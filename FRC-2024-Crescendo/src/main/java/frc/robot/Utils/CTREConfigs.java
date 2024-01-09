a78fc5667a0bfd78ed50c6db19a7fad9f92fe7ac
Braden <4bktandme@gmail.com>
2024-01-08 15:20:18 -1000

    All new Libraries except Phoenix

--- FRC-2024-Crescendo/src/main/java/frc/robot/Utils/CTREConfigs.java
+++ FRC-2024-Crescendo/src/main/java/frc/robot/Utils/CTREConfigs.java
@@ -9,13 +9,14 @@ import com.ctre.phoenix.sensors.CANCoderConfiguration;
 import com.ctre.phoenix.sensors.SensorInitializationStrategy;
 import com.ctre.phoenix.sensors.SensorTimeBase;
 
+
 import frc.robot.Constants;
 
 public final class CTREConfigs {
     public CANCoderConfiguration swerveCanCoderConfig;
 
     public CTREConfigs(){
-        swerveCanCoderConfig = new CANCoderConfiguration();
+        swerveCanCoderConfig = new CANcoderConfigurator();
         
         /* Swerve CANCoder Configuration */
         swerveCanCoderConfig.absoluteSensorRange = AbsoluteSensorRange.Unsigned_0_to_360;
