43a315c5b2b41806932a3c8868b605058402dc23
Spryzen <bkt2401@iolani.org>
2024-03-27 17:49:01 -1000

    Vision aided shooting version 1 done

--- FRC-2024-Crescendo/src/main/java/frc/robot/Subsystems/Vision.java
+++ FRC-2024-Crescendo/src/main/java/frc/robot/Subsystems/Vision.java
@@ -13,6 +13,7 @@ import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.DriverStation;
